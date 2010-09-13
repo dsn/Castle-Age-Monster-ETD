d8d7f12dc6b0f6322191d88451c3d6ac97d553bb
DSN <garys@ussearch.com>
2010-09-13 16:07:16 -0700

    Fixed Typo Removed Polling Option now on Click

--- ca-monster-etd.user.js
+++ ca-monster-etd.user.js
@@ -1,5 +1,5 @@
-// Castle Age Monster Kill Time Estimater
-// version 0.1 ALPHA!
+// Castle Age Monster Kill Time Estimator
+// version 0.3 ALPHA!
 // 2010-09-11
 // Copyright (c) 2010, Dark Side Networks
 // Released under the GPL license
@@ -19,7 +19,7 @@
 // --------------------------------------------------------------------
 //
 // ==UserScript==
-// @name          Castle Age Monster Kill Time Estimater
+// @name          Castle Age Monster Kill Time Estimator
 // @namespace     ca-monster-etd
 // @description   Estimate time left to kill a monster
 // @include       http://apps.facebook.com/castle_age/battle_monster.php*
@@ -55,79 +55,61 @@ var makeTimer = function(sec) {
 };
 
 
-function do_it() {
-var health;
-var health;
-var dmg = 0;
-var imgs = document.getElementsByTagName("img")
-var drgContainer = document.getElementsByClassName("dragonContainer")
-if(drgContainer.length == 1) {
-  for (var i = 0; imgs.length > i; i++) {
-    // Get Health
-    if(imgs[i].src.search(/nm_red\.jpg$/) != -1) {
-       health = imgs[i].parentNode.style.width 
-	}
-    // Get Party Strength / Defense
-    else if(imgs[i].src.search(/nm_green\.jpg$/) != -1) {
-	  strength = imgs[i].parentNode.style.width; 
-	}
-  }
+function pollPage() {
+    var drgContainer = document.getElementsByClassName("dragonContainer")
+    if (drgContainer.length > 0) {
 
-  var dmgRows = drgContainer[0].children[0].children[0].children[0].cells[1].childNodes[1].childNodes[1].childNodes[5].rows;
-  for (var i = 0; dmgRows.length > i; i++) {
-    var dmgData = dmgRows[i].children[2];
-    if(dmgData != undefined) {
-      dmg+= parseInt(dmgData.textContent.replace(/[^0-9\/]/g,'')) 
-	}
-  }
+        var health, strength;
+        var dmg = 0;
+        var imgs = document.getElementsByTagName("img")
+        var drgContainer = document.getElementsByClassName("dragonContainer")
+        if (drgContainer.length == 1) {
+            for (var i = 0; imgs.length > i; i++) {
+                // Get Health
+                if (imgs[i].src.search(/nm_red\.jpg$/) !== -1 || imgs[i].src.search(/monster_health_background\.jpg/) !== -1) {
+                    health = imgs[i].parentNode.style.width
+                }
+                // Get Party Strength / Defense
+                else if (imgs[i].src.search(/nm_green\.jpg$/) !== -1) {
+                    strength = imgs[i].parentNode.style.width.replace("%", "");
+                }
+                else if (imgs[i].src.search(/bar_dispel\.gif$/) !== -1) {
+                    strength = 100 - parseInt(imgs[i].parentNode.style.width.replace("%", ""));
+                }
+            }
+
+            var dmgRows = drgContainer[0].children[0].children[0].children[0].cells[1].childNodes[1].childNodes[1].childNodes[5].rows;
+            for (var i = 0; dmgRows.length > i; i++) {
+                var dmgData = dmgRows[i].children[2];
+                if (dmgData != undefined) {
+                    dmg += parseInt(dmgData.textContent.replace(/[^0-9\/]/g, ''))
+                }
+            }
+        }
+        // Calculations
+        var now = Date.now();
+        var total = Math.ceil(100 * dmg / (health === 100 ? 0.1 : (100 - parseInt(health))));
+        var timer = document.getElementById("app46755028429_monsterTicker").textContent.parseTimer()
+        var dps = dmg / (604800 - timer);
+        var eta = now + (Math.floor((total - dmg) / dps) * 1000);
+        var finish = now + (timer * 1000);
+        // Format Results
+        var data = "&nbsp;&nbsp;<b>Monster Health</b> " + Math.round(health.split("%")[0]) + "%<br/>" + "&nbsp;&nbsp;<b>Party Strength</b> " + Math.round(strength) + "%<br/><br/>" +
+
+        "&nbsp;&nbsp;<b>DPS</b> " + dps.toString().split(".")[0] + "<br/>" + "&nbsp;&nbsp;<b>Total Damage</b> " + addCommas(dmg) + "<br/><br/>" +
+
+        "&nbsp;&nbsp;<b>Remaining</b> " + document.getElementById("app46755028429_monsterTicker").textContent + "<br/>" + "&nbsp;&nbsp;<b>ETD</b> " + makeTimer((eta - Date.now()) / 1000);
+
+        document.getElementById("ca-monster-data").innerHTML = data;
+        if (eta < finish) {
+            document.getElementById("ca-etd-cpanel").getElementsByTagName("h4")[0].style.background = "green";
+        } else {
+            document.getElementById("ca-etd-cpanel").getElementsByTagName("h4")[0].style.background = "red";
+        }
+
+    }
 }
 
-// Calculations
-var now = Date.now();
-var total = Math.ceil(100 * dmg / (health === 100 ? 0.1 : (100 - parseInt(health))));
-var timer = document.getElementById("app46755028429_monsterTicker").textContent.parseTimer()
-var dps = dmg / (604800 - timer);
-var eta =  now + (Math.floor((total - dmg) / dps) * 1000);
-var finish = now + (timer * 1000);
-// Format Results
-var data = "&nbsp;&nbsp;<b>Monster Health</b> " + Math.round(health.split("%")[0]) + "%<br/>" + 
-			"&nbsp;&nbsp;<b>Party Strength</b> " + Math.round(strength.split("%")[0]) + "%<br/><br/>" + 
-
-			"&nbsp;&nbsp;<b>DPS</b> " + dps.toString().split(".")[0] + "<br/>" +
-			"&nbsp;&nbsp;<b>Total Damage</b> " + addCommas(dmg) + "<br/><br/>" + 
-
-			"&nbsp;&nbsp;<b>Remaining</b> " + document.getElementById("app46755028429_monsterTicker").textContent + "<br/>" +
-			"&nbsp;&nbsp;<b>ETD</b> " + makeTimer((eta - Date.now()) / 1000);
-
-document.getElementById("ca-monster-data").innerHTML = data;
-if(eta < finish) { 
-	document.getElementById("ca-etd-cpanel").getElementsByTagName("h4")[0].style.background = "green";
-} else {
-	document.getElementById("ca-etd-cpanel").getElementsByTagName("h4")[0].style.background = "red";
-}
-
-}
-function pollPage (intId) {
-	console.log(intId)
-	var drgContainer = document.getElementsByClassName("dragonContainer")
-		if (drgContainer.length > 0) {
-				clearInterval(intId);
-				setTimeout(function () { doTimer(); }, 30000);
-				do_it();
-		}
-}
-
-function timer(intId) { 
-	
-	var drgContainer = document.getElementsByClassName("dragonContainer")
-		if(drgContainer.length > 0) {
-				pollPage(intId);
-		} 
-}
-
-function doTimer() {
-var intId = setInterval(function () { timer(intId); }, 3000);
-}
 
 function addGlobalStyle (css) {
             var head, style;
@@ -159,6 +141,11 @@ addGlobalStyle(
 				'  z-index: 99999;' +
 				'  font-family: Verdana, sans-serif; }' +
 
+			'#ca-etd-cpanel a {' +
+			    '  padding-left: 10px;' +
+			    '  color: white;' +
+				'  font-weight: bold;}' +
+
 			'#ca-etd-cpanel h4 {' +
 				' -moz-border-radius-topright: 5px;' +
 				' -moz-border-radius-topleft: 5px;' +
@@ -170,10 +157,11 @@ addGlobalStyle(
 				'  color: white; }');
 
 var indent = "&nbsp;&nbsp;"
-desc = "<h4>Monster Death Estimator</h4><br/><span id='ca-monster-data'>Visit Monster to view Data</span>";
+desc = "<h4>Monster Death Estimator</h4><br/><span id='ca-monster-data'><a href='javascript:void(0);' id='pollMonster' title='Check Monster'>Check Monster</a></span>";
 div = document.createElement('div');
 div.id = 'ca-etd-cpanel';
 div.innerHTML = desc;
 document.body.style.paddingBottom = "4em";
 document.body.appendChild(div);
-doTimer();
+var btn = document.getElementById("pollMonster")
+btn.addEventListener("click", function () { pollPage(); }, true);
